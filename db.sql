6330d4d18ad0784317ab00ae7b3008943bb364ee
Ubuntu <jordanmurray@CloudComputeModule.uamltmsvdgru3arkgzn1sgqqja.syx.internal.cloudapp.net>
2023-12-06 14:06:10 +0000

    part 2 app

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 CREATE USER 'A'@'%' IDENTIFIED BY 'B';
 GRANT ALL PRIVILEGES ON *.* to 'A'@'%';
 
-CREATE DATABASE student; 
+CREATE DATABASE IF NOT EXISTS student; 
 USE student;
 CREATE TABLE students (studentName VARCHAR(255), email VARCHAR(255), studentID INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY(studentID));
@@ -9,4 +9,3 @@ PRIMARY KEY(studentID));
 INSERT INTO students (studentName, email) values ("first student", "firststudent@mydomain.ie"); 
 INSERT INTO students (studentName, email) values ("second student", "secondstudent@mydomain.ie ");
 SELECT * FROM students;
-exit;
